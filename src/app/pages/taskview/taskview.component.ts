9396bab946e943acef857280b8dd7f750d0ccf13
Stephen Brown <stevanion2013@gmail.com>
2023-03-20 18:11:42 +0100

    task manager update

--- src/app/pages/taskview/taskview.component.ts
+++ src/app/pages/taskview/taskview.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-taskview',
   templateUrl: './taskview.component.html',
-  styleUrls: ['./taskview.component.css']
+  styleUrls: ['./taskview.component.scss']
 })
 export class TaskviewComponent implements OnInit {
 
-  constructor(private taskService: TaskService ) {}
-  
-  ngOnInit(){}
-  
-  createNewList(){
+  lists: List[];
+  tasks: Task[];
 
+  selectedListId: string;
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) {}
+  
+  ngOnInit(){
+    this.params.subscribe(
+      (params: Params) => {
+        if (params.listId) {
+          this.selectedListId = params.listId;
+          this.taskService.getTask(params.listId).subscribe((tasks: Task[]) => {
+            this.tasks = tasks;
+          } ) 
+        } else {
+          this.tasks = undefined;
+        }
+      }
+    )
+  }
+  createNewList(title: string){
+    // we want to send a web request to create a list
+    this.webRequestService.post('/list', { title });
+
+  
   }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -15,7 +15,39 @@ export class TaskService {
 
   createList(Title: string) {
     // we want to send a web request to create a list
-  
+   return this.webRequestService.post('lists', { Title });   
+  }
+
+  updateList(id: string, title: string) {
+    //we want to send a web request to update a list
+    return this.webRequestService.patch(`lists/${id}`, { Title });
+  }
+
+  updateTask(listId: string, taskId: string, title: string) {
+    // we want to send a web request to update a list
+    return this.webRequestService.patch(`lists/${listId}/tasks/${taskId}`, {Title});
+  }
+
+  deleteTask(listId: string, taskId: string) {
+    return this.webRequestService.delete(`lists/${listId}/task/${taskId}`);
+  }
+
+  deleteList(id: string) {
+    return this.webRequestService.delete(`lists/${id}`);
+  }
+  getTask(id: string) {
+    return this.webRequestService.get(`lists/${listId}/tasks`);
+  }
+
+  createTask(title: string, listId: string) {
+    // we want to send a web request to create a task
+    return this.webRequestService.post(`lists/${listId}/tasks`, {title})
+  ;  }
+
+  complete(task: Task){
+    return this.webRequestService.patch(`lists/${task._listId}/tasks/${task._id}`,{
+      completed: !task.completed
+    });
   }
 }
 
